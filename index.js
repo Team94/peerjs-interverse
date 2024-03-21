ab10f7466cdb47261d4a5957568ed081647372ad
kangkyeongseo <kks_big@naver.com>
2024-03-21 15:28:42 +0900

    Chore: 서버 포트 수정

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //Peer server
-var privateKey = fs.readFileSync("sslcert/server.key", "utf8");
-var certificate = fs.readFileSync("sslcert/server.crt", "utf8");
+// var privateKey = fs.readFileSync("sslcert/server.key", "utf8");
+// var certificate = fs.readFileSync("sslcert/server.crt", "utf8");
 
 const { PeerServer } = require("peer");
 PeerServer({
   port: 443,
   path: "/",
-  ssl: {
-    key: privateKey,
-    cert: certificate,
-  },
+  // ssl: {
+  //   key: privateKey,
+  //   cert: certificate,
+  // },
 });
