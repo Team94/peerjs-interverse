59e3d7fefb9b07356e45bc0164044a1c17d9f919
geonwooPark <white0581@naver.com>
2024-06-12 00:31:29 +0900

    Chore: 기타 코드 수정

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 import { PeerServer } from "peer";
 
-var privateKey = fs.readFileSync("sslcert/server.key", "utf8");
-var certificate = fs.readFileSync("sslcert/server.crt", "utf8");
+PeerServer({ port: 9001, path: "/" });
 
-PeerServer({
-  port: 443,
-  path: "/",
-  ssl: {
-    key: privateKey,
-    cert: certificate,
-  },
-});
 console.log("PeerJS 서버 실행중..");
